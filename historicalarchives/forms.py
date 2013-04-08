3ab8a9dea2340e8a83e736d3b2c86d437c00ed06
mieszko chowaniec <mieszko.chowaniec@gmail.com>
2013-04-08 10:25:21 +0200

    #29 - poprawa tabsow na spacje

--- historicalarchives/forms.py
+++ historicalarchives/forms.py
@@ -2,8 +2,8 @@ from django import forms
 import datetime
 
 class UploadMaterial(forms.Form)
-	title  = forms.CharField(max_length = 100, help_text = 'Tytul')
-	description = forms.TextField(help_text = 'Opis')
-	date = forms.DateField(initial=datetime.date.today)
+    title  = forms.CharField(max_length = 100, help_text = 'Tytul')
+    description = forms.TextField(help_text = 'Opis')
+    date = forms.DateField(initial=datetime.date.today)
     #do zastnowienia place = 
-	file = forms.FileField()
+    file = forms.FileField()
