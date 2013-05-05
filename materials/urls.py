054dc5cbdda7463d2639c0feac1577123be57a86
m <m@mw.(none)>
2013-05-05 15:39:53 +0200

    few more views

--- materials/urls.py
+++ materials/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('materials.views',
         url(r'^search$', 'SearchForMaterial', name = 'search'),
-        url(r'^details$', 'DetailsOfMaterials', name = 'details'),
+        url(r'^details/(?P<pk>\d+)$', 'DetailsOfMaterials', name = 'details'),
         url(r'^add$', 'AddMaterial', name = 'add'),
+        url(r'^viewnew$', 'ViewNewMaterials', name = 'viewnew'),
         url(r'^request_add$', 'RequestMaterial', name = 'request_add'),
         url(r'^requests$', 'ViewAllRequests', name = 'requests'),
-        url(r'^request_details$', 'DetailsOfRequest', name = 'reqeust_detail'),
-        url(r'^collection_build$', 'BuildCollection', name='collection_build'),
+        url(r'^request_details/(?P<pk>\d+)$', 'DetailsOfRequest', name = 'reqeust_detail'),
+        url(r'^collection_build$'/(?P<pk>\d+), 'BuildCollection', name='collection_build'),
         url(r'^collection_details$', 'DetailsOfCollection', name='collection_details'),
         url(r'^collections$', 'ViewAllCollections', name='collections'),
         )
--- materials/views.py
+++ materials/views.py
@@ -1,28 +1,44 @@
 from django.shortcuts import render_to_response
+from materials.models import Material, Request_for_materials
 
 def SearchForMaterial(request):
     return render_to_response('materials/materials_search.html')
 
-def DetailsOfMaterials(request):
-    return render_to_response("materials/material_details.html")
+def DetailsOfMaterials(request, pk):
+    material = Material.objects.get(pk = pk)
+    return render_to_response("materials/material_details.html", {
+        'material': material})
 
 def AddMaterial(request):
     return render_to_response("materials/material_add.html")
+
+def ViewNewMaterials(request):
+    materials = Material.objects.all().order_by('date_of_creation')[:10]
+    return render_to_response("materials/view_new_materials.html", {
+        "materials": materials})
 
 def RequestMaterial(request):
     return render_to_response("materials/request_issue_new.html")
 
 def ViewAllRequests(request):
-    return render_to_response("materials/requests.html")
+    requests = Request_for_materials.objects.all()
+    return render_to_response("materials/requests.html", {
+        'requests_list':requests})
 
-def DetailsOfRequest(request):
-    return render_to_response("materials/request_details.html")
+def DetailsOfRequest(request, pk):
+    request_for_materials = Request_for_materials.objects.get(pk = pk)
+    return render_to_response("materials/request_details.html", {
+        'request': request_for_materials})
 
 def BuildCollection(request):
     return render_to_response("materials/collection_build.html")
 
-def DetailsOfCollection(request):
-    return render_to_response("materials/collection_details.html")
+def DetailsOfCollection(request, pk):
+    collection =Collection.objects.get(pk = pk)
+    return render_to_response("materials/collection_details.html", {
+        "collection": collection})
 
 def ViewAllCollections(request):
-    return render_to_response("materials/collections.html")
+    collections = Collection.objects.all()
+    return render_to_response("materials/collections.html", {
+        "collections": collections}))
