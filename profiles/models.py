7d57dd75c297601344db707f1ffb9adfba9544da
M. Chabiera <m.drakhu@gmail.com>
2013-05-06 14:18:23 +0300

    Little update.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  """ Common user profile """
+	
+    login = models.CharField(max_length = 25, unique = True)
+    #password = 
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 45)
+    birth_date = models.DateField()
+    GENDER_CHOICES = (
+    	('Male', 'M'),
+    	('Female', 'F'))
+    gender = models.CharField(choices = GENDER_CHOICES)
+    mail =	models.EmailField(max_length = 75, unique = True)
