b90cae6f4f772140ccb0098a79d1eab44ed46810
M. Chabiera <m.drakhu@gmail.com>
2013-05-10 14:01:54 +0300

    Little update on user model.

--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):
+class CommonUser(models.Model):
     """ Common user profile """	
-    login = models.CharField(max_length = 25, unique = True)
-    #password = 
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 45)
+    user = models.OneToOneField(User)
     birth_date = models.DateField()
     GENDER_CHOICES = (
     	('Male', 'M'),
     	('Female', 'F'))
     gender = models.CharField(choices = GENDER_CHOICES, max_length = 6)
-    mail = models.EmailField(max_length = 75, unique = True)
